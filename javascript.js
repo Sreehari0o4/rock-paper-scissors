ee7b9205883e9c267d924128f4c4a8923c91947e
Sreehari0o4 <SreehariwrKo8@gmail.com>
2024-05-21 11:07:42 +0530

    Add skeletel structure of function

--- javascript.js
+++ javascript.js
@@ -3,3 +3,29 @@
 //comapre and print + update score
 //repeat for 5 times
 //end print the winner
+//everything in a function playGame() except winner print
+//playGame() in for loop
+// score is global
+
+let humanScore=0;
+let computerScore=0;
+
+for(i=1;i<6;i++)
+    {
+        playGame();
+    }
+
+function playGame()
+{
+    function getHumanChoice()
+    {
+
+    }
+
+    function getComputerChoice()
+    {
+
+    }
+
+    
+}
